5865e24f5f111a17d15fecf9948255ee193352f6
Krishna Dalsania <kdalsania@officebrain.com>
2017-12-07 11:35:58 +0530

    Initial commit

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -1,25 +1,98 @@
- <template>
-  <!-- <div>
-    <f-header></f-header>
-          <router-view></router-view>
-    <f-footer></f-footer>
-  </div> -->
+<style scoped>
+    .layout{
+        border: 1px solid #d7dde4;
+        background: #f5f7f9;
+        position: relative;
+        border-radius: 4px;
+        overflow: hidden;
+    }
+    .layout-breadcrumb{
+        padding: 10px 15px 0;
+    }
+    .layout-content{
+        min-height:800px;
+        margin: 15px;
+        overflow: hidden;
+        background: #010a0f;
+        border-radius: 4px;
+    }
+    .layout-content-main{
+        padding: 10px;
+    }
+    .layout-copy{
+        text-align: left;
+        padding: 10px 0 20px;
+        color: #9ea7b4;
+    }
+    .layout-menu-left{
+        background: #010a0f;
+        color: #fff;
+    }
+    .layout-header{
+        height: 60px;
+        background: #2a3f54;
+        box-shadow: 0 1px 1px rgba(0,0,0,.1);
+        text-align: left;
+    }
+    .layout-logo-left
+    {
+        width: 86%;
+        height: 30px;
+        background: #5b6270;
+        border-radius: 3px;
+        margin: 15px auto;
+        text-align: left;
+        padding: 0px 11px;
+    }
+    
+    .layout-ceiling-main a{
+        color: #9ba7b5;
+    }
+    .layout-hide-text .layout-text{
+        display: none;
+    }
+    .ivu-col{
+        transition: width .2s ease-in-out;
+    }
+    .ivu-menu-dark {
+    background: #010a0e;
+    }
+    .ivu-menu li{
+        text-align: left;
+        
+    }
+    .ivu-menu-submenu-title span{
+        
+        color: #fefefe;
+        font-size: 17px;
+        font-weight: 500;
+        letter-spacing: 0.4px;
+    }
+
+    .ivu-menu-submenu ivu-menu-item-active ivu-menu-opened{
+        background-color: #010a0e;
+    }
 
 
-  <div class="layout" :class="{'layout-hide-text': spanLeft < 5}">
+   
+.ivu-menu-dark.ivu-menu-vertical .ivu-menu-submenu .ivu-menu-item-active, .ivu-menu-dark.ivu-menu-vertical .ivu-menu-submenu .ivu-menu-item-active:hover {
+    border-right: none;
+    color: #fff;
+    background: #091e33!important;
+}
+</style>
+<template>
+    <div class="layout" :class="{'layout-hide-text': spanLeft < 5}">
         <Row type="flex">
             <Col :span="spanLeft" class="layout-menu-left">
-                <Menu active-name="1" theme="dark" width="auto">
-                    <div class="layout-logo-left"></div>
-                    <MenuItem name="0">
-                          <Button type="text" @click="toggleClick">
-                          <Icon type="navicon" size="32"></Icon>
-                      </Button>
-                    </MenuItem>
-                     <MenuItem name="1">
+                <!-- <Menu active-name="1" theme="dark" width="auto">
+                    <div class="layout-logo-left">
+                        <img src="../assets/images/Flowz.svg" style="width: 85px;">
+                    </div>
+                   
+                    <MenuItem name="1">
                         <Icon type="ios-navigate" :size="iconSize"></Icon>
-                        <span class="layout-text">Option 1</span>
-                      </Button>
+                        <span class="layout-text">Dashboard</span>
                     </MenuItem>
                     <MenuItem name="2">
                         <Icon type="ios-keypad" :size="iconSize"></Icon>
@@ -28,22 +101,55 @@
                     <MenuItem name="3">
                         <Icon type="ios-analytics" :size="iconSize"></Icon>
                         <span class="layout-text">Option 3</span>
-                    </MenuItem> -->
-                    
-                </Menu>
+                    </MenuItem>
+                </Menu> -->
+                
+                <Menu active-name="1-2" width="auto"  theme="dark" >
+                    <div class="layout-logo-left">
+                        <img src="../assets/images/Flowz.svg" style="width: 85px;">
+                    </div>
+                        <Submenu name="1">
+                            <template slot="title">
+                                <Icon type="ios-navigate" :size="iconSize"></Icon>
+                                 <span class="layout-text">Dashboard</span>
+                            </template>
+                            
+                        </Submenu>
+                        <Submenu name="2">
+                            <template slot="title">
+                                <Icon type="ios-keypad" :size="iconSize"></Icon>
+                                <span class="layout-text"> CRM </span>
+                            </template>
+                            <MenuItem name="2-1">New CRM</MenuItem>
+                            <MenuItem name="2-2">CRM List</MenuItem>
+                        </Submenu>
+                        <Submenu name="3">
+                            <template slot="title">
+                                <Icon type="ios-analytics" :size="iconSize"></Icon>
+                                <span class="layout-text">  Accounting </span>
+                            </template>
+                            <MenuItem name="3-1">Invoice</MenuItem>
+                            <MenuItem name="3-2">Payment</MenuItem>
+                        </Submenu>
+                    </Menu>
             </Col>
             <Col :span="spanRight">
                 <div class="layout-header">
-                      <Button type="text" @click="toggleClick">
-                          <Icon type="navicon" size="32"></Icon>
-                      </Button>
-                    <f-header></f-header>
+                    <Button type="text" @click="toggleClick">
+                        <Icon type="navicon" color=#fff size="32"></Icon>
+                    </Button>
+                    <div style="float:right">
+                    <Button type="text"  @click="toggleClick">
+                        <Icon type="bathtub" color=#fff size="32"></Icon>
+                    </Button>
+                    </div>
                 </div>
+               
                 <div class="layout-content">
-                   <router-view></router-view>
+                    <router-view></router-view>
                 </div>
                 <div class="layout-copy">
-                   <f-footer></f-footer>
+                    2011-2016 &copy; TalkingData
                 </div>
             </Col>
         </Row>
@@ -52,17 +158,16 @@
 <script>
 import Header from './Header'
 import Footer from './Footer'
-export default {
-  name: 'app',
-  components: {
-    'f-header': Header,
-    'f-footer': Footer
-  },
-  data () {
+    export default {
+        data () {
             return {
                 spanLeft: 5,
                 spanRight: 19
             }
+        },
+        components: {
+            'f-header': Header,
+            'f-footer': Footer
         },
         computed: {
             iconSize () {
@@ -80,57 +185,5 @@ export default {
                 }
             }
         }
-}
+    }
 </script>
-
-<<style scoped>
-.layout{
-        border: 1px solid #d7dde4;
-        background: #f5f7f9;
-        position: relative;
-        border-radius: 4px;
-        overflow: hidden;
-    }
-    .layout-breadcrumb{
-        padding: 10px 15px 0;
-    }
-    .layout-content{
-        min-height: 200px;
-        margin: 15px;
-        overflow: hidden;
-        background: #fff;
-        border-radius: 4px;
-    }
-    .layout-content-main{
-        padding: 10px;
-    }
-    .layout-copy{
-        text-align: center;
-        padding: 10px 0 20px;
-        color: #9ea7b4;
-    }
-    .layout-menu-left{
-        background: #464c5b;
-    }
-    .layout-header{
-        height: 60px;
-        background: #fff;
-        box-shadow: 0 1px 1px rgba(0,0,0,.1);
-    }
-    .layout-logo-left{
-        width: 90%;
-        height: 30px;
-        background: #5b6270;
-        border-radius: 3px;
-        margin: 15px auto;
-    }
-    .layout-ceiling-main a{
-        color: #9ba7b5;
-    }
-    .layout-hide-text .layout-text{
-        display: none;
-    }
-    .ivu-col{
-        transition: width .2s ease-in-out;
-    }
-</style>
