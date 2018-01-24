8cbced794f876a0c05837d988da0ec52f6893803
avasani <avasani@officebrain.com>
2018-01-25 04:04:15 +0530

    moved to https

--- src/config/customConfig.js
+++ src/config/customConfig.js
@@ -2,22 +2,22 @@
 // console.log(process.env.domainkey)
 // module.exports =  {
 //     default : {
-//         loginUrl : "http://auth.flowz.com/api/login",
-//         ldapLoginUrl : "http://auth.flowz.com/api/ldapauth",
-//         registrationUrl : "http://api.flowz.com/serverapi/register",
-//         signupUrl:          "http://auth.flowz.com/api/setup",
-//         userDetail:             "http://auth.flowz.com/api/userdetails",
-//         forgotPasswordUrl :         "http://auth.flowz.com/api/forgetpassword",
-//         resetpassword :                 "http://auth.flowz.com/api/resetpassword",
-//         feathersServiceBaseUrl :            "http://api.flowz.com/serverapi/",
-//         facebookSuccessCallbackUrl :            "http://dashboard.flowz.com",
-//         loginWithFacebookUrl :                      "http://auth.flowz.com/auth/facebook",
-//         googleSuccessCallbackUrl :                      "http://dashboard.flowz.com",
-//         loginWithGoogleUrl :                                "http://auth.flowz.com/auth/Gplus",
-//         serviceUrl :                                            "http://api.flowz.com/crm/",
-//        //serviceUrl :                                                "http://localhost:3037/",
-//         projecturl :                                                       'http://api.flowz.com/mom/',
-//         assigneeapi :                                                           'http://api.flowz.com/user/alluserdetails/'
+//         loginUrl : "https://auth.flowz.com/api/login",
+//         ldapLoginUrl : "https://auth.flowz.com/api/ldapauth",
+//         registrationUrl : "https://api.flowz.com/serverapi/register",
+//         signupUrl:          "https://auth.flowz.com/api/setup",
+//         userDetail:             "https://auth.flowz.com/api/userdetails",
+//         forgotPasswordUrl :         "https://auth.flowz.com/api/forgetpassword",
+//         resetpassword :                 "https://auth.flowz.com/api/resetpassword",
+//         feathersServiceBaseUrl :            "https://api.flowz.com/serverapi/",
+//         facebookSuccessCallbackUrl :            "https://dashboard.flowz.com",
+//         loginWithFacebookUrl :                      "https://auth.flowz.com/auth/facebook",
+//         googleSuccessCallbackUrl :                      "https://dashboard.flowz.com",
+//         loginWithGoogleUrl :                                "https://auth.flowz.com/auth/Gplus",
+//         serviceUrl :                                            "https://api.flowz.com/crm/",
+//        //serviceUrl :                                                "https://localhost:3037/",
+//         projecturl :                                                       'https://api.flowz.com/mom/',
+//         assigneeapi :                                                           'https://api.flowz.com/user/alluserdetails/'
 //     }
 
 // }
@@ -25,21 +25,21 @@
 
 console.log("???????????????????????? " ,process.env.domainkey)
 
-//let frontEndUrl = 'http://localhost:8081/#/login';
-let frontEndUrl = 'http://crm.'+process.env.domainkey+'/#/login';
+//let frontEndUrl = 'https://localhost:8081/#/login';
+let frontEndUrl = 'https://crm.'+process.env.domainkey+'/#/login';
 
 module.exports =  {
     default : {
 
-        loginUrl : "http://auth."+process.env.domainkey+"/api/login",
-        ldapLoginUrl : "http://auth."+process.env.domainkey+"/api/ldapauth",
-        registrationUrl : "http://api."+process.env.domainkey+"/serverapi/register",
-        signupUrl:          "http://auth."+process.env.domainkey+"/api/setup",
-        userDetail:             "http://auth."+process.env.domainkey+"/api/userdetails",
-        forgotPasswordUrl :         "http://auth."+process.env.domainkey+"/api/forgetpassword",
-        resetpassword :                 "http://auth."+process.env.domainkey+"/api/resetpassword",
-        feathersServiceBaseUrl :            "http://api."+process.env.domainkey+"/serverapi/",
-        varifyEmailUrl :                    "http://auth."+process.env.domainkey+"/api/verifyemail",
+        loginUrl : "https://auth."+process.env.domainkey+"/api/login",
+        ldapLoginUrl : "https://auth."+process.env.domainkey+"/api/ldapauth",
+        registrationUrl : "https://api."+process.env.domainkey+"/serverapi/register",
+        signupUrl:          "https://auth."+process.env.domainkey+"/api/setup",
+        userDetail:             "https://auth."+process.env.domainkey+"/api/userdetails",
+        forgotPasswordUrl :         "https://auth."+process.env.domainkey+"/api/forgetpassword",
+        resetpassword :                 "https://auth."+process.env.domainkey+"/api/resetpassword",
+        feathersServiceBaseUrl :            "https://api."+process.env.domainkey+"/serverapi/",
+        varifyEmailUrl :                    "https://auth."+process.env.domainkey+"/api/verifyemail",
 
 
 
@@ -50,21 +50,21 @@ module.exports =  {
         linkedInSuccessCallbackUrl: frontEndUrl,
 
 
-        flowzDashboardUrl : 'http://dashboard.'+process.env.domainkey,
-        flowzBuilderUrl : 'http://webbuilder.'+process.env.domainkey ,
-        flowzVmailUrl : 'http://vmail.'+process.env.domainkey ,
-        flowzUploaderUrl : 'http://uploader.'+process.env.domainkey ,
-        flowzDbetlUrl : 'http://dbetl.'+process.env.domainkey ,
+        flowzDashboardUrl : 'https://dashboard.'+process.env.domainkey,
+        flowzBuilderUrl : 'https://webbuilder.'+process.env.domainkey ,
+        flowzVmailUrl : 'https://vmail.'+process.env.domainkey ,
+        flowzUploaderUrl : 'https://uploader.'+process.env.domainkey ,
+        flowzDbetlUrl : 'https://dbetl.'+process.env.domainkey ,
 
-        loginWithFacebookUrl : 'http://auth.'+process.env.domainkey+'/auth/facebook',
-        loginWithGoogleUrl : 'http://auth.'+process.env.domainkey+'/auth/Gplus',
-        loginWithTwitterUrl : 'http://auth.'+process.env.domainkey+'/auth/twitter',
-        loginWithGithubUrl : 'http://auth.'+process.env.domainkey+'/auth/github',
-        loginWithLinkedInUrl : 'http://auth.'+process.env.domainkey+'/auth/linkedin',
-        //serviceUrl :            'http://api.'+process.env.domainkey+'/crm/',
-        serviceUrl :            'http://localhost:3037/',
-        projecturl :            'http://api.'+process.env.domainkey+'/mom/',
-        assigneeapi :           'http://api.'+process.env.domainkey+'/user/alluserdetails/'
+        loginWithFacebookUrl : 'https://auth.'+process.env.domainkey+'/auth/facebook',
+        loginWithGoogleUrl : 'https://auth.'+process.env.domainkey+'/auth/Gplus',
+        loginWithTwitterUrl : 'https://auth.'+process.env.domainkey+'/auth/twitter',
+        loginWithGithubUrl : 'https://auth.'+process.env.domainkey+'/auth/github',
+        loginWithLinkedInUrl : 'https://auth.'+process.env.domainkey+'/auth/linkedin',
+        //serviceUrl :            'https://api.'+process.env.domainkey+'/crm/',
+        serviceUrl :            'https://localhost:3037/',
+        projecturl :            'https://api.'+process.env.domainkey+'/mom/',
+        assigneeapi :           'https://api.'+process.env.domainkey+'/user/alluserdetails/'
 
     }
 
