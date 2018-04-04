3c8ff4c64ea824a907f786b3f456b441cdd89a65
Krishna Dalsania <kdalsania@officebrain.com>
2018-04-04 17:46:46 +0530

    UI change in request quote, label and value change for payment credentials

--- src/views/admin-account/view-request-info.vue
+++ src/views/admin-account/view-request-info.vue
@@ -11,151 +11,176 @@
           </div>
         </div>
       </div>
-      <div style="text-align:center">
-        <div class="row">
-          <div class="col-sm-12">
-            <div class="order-title">
-              <div class="col-lg-3 col-lg-offset-2 col-md-4 col-sm-4">
-                ID : {{row.id}}
-              </div>
-              <div class="col-lg-3 col-md-4 col-sm-4">
-                Requested On : {{created_date}}
-              </div>
-              <div class="col-lg-2 col-md-4 col-sm-4">
-                Total Items : {{row.productInfo.length}}
+      <div class="row">
+        <div class="col-sm-6 right-border">
+          <div style="text-align:center">
+            <div class="row">
+              <div class="col-sm-12">
+                <div class="order-title">
+                  <div class="border-white col-lg-6 col-md-6 col-sm-6">
+                    ID : {{row.id}}
+                  </div>
+                  <div class="border-white col-lg-3 col-md-3 col-sm-3">
+                    Requested On : {{created_date}}
+                  </div>
+                  <div class="col-lg-3 col-md-3 col-sm-3">
+                    Total Items : {{row.productInfo.length}}
+                  </div>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </div>
-      <Collapse accordion>
-        <Panel name="1">
-          PRODUCT
-          <p slot="content">
-            <label style="width:-webkit-fill-available;">
-          <div class="panel-body" v-for="item in row.productInfo">
-            <div class="row">
-              <div class="col-lg-2 col-md-3 col-sm-12 col-xs-12 padding-right-0">
-                <div class="detail-image">
-                  <img alt="" :src="getImgUrl(item.default_image)" id="order_product_image_0" class="img-responsive">
-                  </div>
-                </div>
-                <div class="col-lg-10 col-md-9 col-sm-12 col-xs-12" style="text-align: -webkit-center;">
-                  <h2 class="heading-2">{{item.product_name}}</h2>
-                  <div class="skuprice">
-                    <div style="text-align: -webkit-center;background: #f5f5f5;padding: 7px 0;">
-                      Item Code :
-                      <span>{{item.sku}}</span>
-                    </div>
-                  </div>
-                </div>
-            </div>
-            <div v-for="i in item.pricing">
-              <div v-if="i.price_type == 'regular' && i.type == 'decorative' && i.global_price_type == 'global'">
-                <h3 class="quantity-price">Quantity Price</h3>
-                <div class="quantity-table">
-                  <div class="quantity-table-title" style="float: left;width: 23%;">
-                    <div class="table-heading js-product-price-unit" data-product-price-unit="each">Quantity </div>
-                    <div class="table-content">Price</div>
-                  </div>
-                  <div class="quantity-table-disc">
-                    <div class="quantity-table-col owl-carousel owl-theme" style="opacity: 1; display: block;">
-                      <div class="owl-wrapper-outer" v-for="element in i.price_range">
-                        <ul class="owl-wrapper ulList" style="width: 808px; left: 0px; display: block; transition: all 0ms ease; transform: translate3d(0px, 0px, 0px);">
-                          <li class="owl-item" style="width: 101px;">
-                            <div>
-                              <div class="table-heading" v-if="element.qty.lte">{{element.qty.gte}} - {{element.qty.lte}}</div>
-                              <div class="table-heading" v-else>{{element.qty.gte}} + </div>
-                              <div class="table-content"> {{accounting(element.price)}}</div>
-                            </div>
-                          </li>
-                        </ul>
+          <Collapse accordion>
+            <Panel name="1">
+              PRODUCT
+              <p slot="content">
+                <label style="width:-webkit-fill-available;">
+              <div style="padding-right:15px" v-for="item in row.productInfo">
+                <div class="row">
+                  <div class="col-lg-2 col-md-3 col-sm-12 col-xs-12 padding-right-0">
+                    <div class="detail-image">
+                      <img alt="" :src="getImgUrl(item.default_image)" id="order_product_image_0" class="img-responsive">
                       </div>
-                      <div class="owl-controls clickable" style="display: none;">
-                        <div class="owl-pagination">
-                          <div class="owl-page active">
-                            <span class=""></span>
+                    </div>
+                    <div class="col-lg-10 col-md-9 col-sm-12 col-xs-12" style="text-align: -webkit-center;">
+                      <h2 class="heading-2" style="font-size:20px !important">{{item.product_name}}</h2>
+                      <div class="skuprice">
+                        <div style="text-align: -webkit-center;background: #f5f5f5;padding: 7px 0;">
+                          Item Code :
+                          <span>{{item.sku}}</span>
+                        </div>
+                      </div>
+                    </div>
+                </div>
+                <div v-for="i in item.pricing">
+                  <div v-if="i.price_type == 'regular' && i.type == 'decorative' && i.global_price_type == 'global'">
+                    <h3 class="quantity-price">Quantity Price</h3>
+                    <div class="quantity-table">
+                      <div class="quantity-table-title" style="float: left;width: 23%;">
+                        <div class="table-heading js-product-price-unit" data-product-price-unit="each">Quantity </div>
+                        <div class="table-content">Price</div>
+                      </div>
+                      <div class="quantity-table-disc">
+                        <div class="quantity-table-col owl-carousel owl-theme" style="opacity: 1; display: block;">
+                          <div class="owl-wrapper-outer" v-for="element in i.price_range">
+                            <ul class="owl-wrapper ulList" style="width: 808px; left: 0px; display: block; transition: all 0ms ease; transform: translate3d(0px, 0px, 0px);">
+                              <li class="owl-item" style="width: 101px;">
+                                <div>
+                                  <div class="table-heading" v-if="element.qty.lte">{{element.qty.gte}} - {{element.qty.lte}}</div>
+                                  <div class="table-heading" v-else>{{element.qty.gte}} + </div>
+                                  <div class="table-content"> {{accounting(element.price)}}</div>
+                                </div>
+                              </li>
+                            </ul>
+                          </div>
+                          <div class="owl-controls clickable" style="display: none;">
+                            <div class="owl-pagination">
+                              <div class="owl-page active">
+                                <span class=""></span>
+                              </div>
+                            </div>
+                            <div class="owl-buttons">
+                              <div class="owl-prev"></div>
+                              <div class="owl-next"></div>
+                            </div>
                           </div>
                         </div>
-                        <div class="owl-buttons">
-                          <div class="owl-prev"></div>
-                          <div class="owl-next"></div>
-                        </div>
                       </div>
+                      <div class="clearfix"></div>
                     </div>
                   </div>
-                  <div class="clearfix"></div>
                 </div>
-              </div>
-            </div>
-            <h3 class="product-summary">Colours</h3>
-              <div class="estimate-detail product-section-box">
-                <div class="estimate-tag-block border-bottom">
-                  <div class="row">
-                    <div class="col-sm-12">
-                      <div class="inquiry-color-block">
-                        <ul class="ulList" v-for="i in item.attributes.colors">
-                          <li class="color2" data-original-title="i" :style="{ backgroundColor: i }"></li>
-                        </ul>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="estimate-tag-block">
-                    <h3 class="">Product Description</h3>
+                <h3 class="product-summary">Colours</h3>
+                  <div class="estimate-detail product-section-box">
+                    <div class="estimate-tag-block border-bottom">
                       <div class="row">
                         <div class="col-sm-12">
-                          <div class="estimate-row" v-html="item.description"></div>
+                          <div class="inquiry-color-block">
+                            <ul class="ulList" v-for="i in item.attributes.colors">
+                              <li class="color2" data-original-title="i" :style="{ backgroundColor: i }"></li>
+                            </ul>
+                          </div>
                         </div>
                       </div>
+                      <div class="estimate-tag-block">
+                        <h3 class="">Product Description</h3>
+                          <div class="row">
+                            <div class="col-sm-12">
+                              <div class="estimate-row" v-html="item.description"></div>
+                            </div>
+                          </div>
+                        </div>
                     </div>
-                </div>
-              <h3>Special Instructions</h3>
-              <div class="estimate-tag-block">
-                <div class="row">
-                  <div class="col-sm-12">
-                    {{row.instruction}}
+                  <h3>Special Instructions</h3>
+                  <div class="estimate-tag-block">
+                    <div class="row">
+                      <div class="col-sm-12">
+                        {{row.instruction}}
+                      </div>
+                    </div>
+                  </div>
                   </div>
                 </div>
-              </div>
-              </div>
-            </div>
-            </label>
-          </p>
-        </Panel>
-        <Panel name="2">
-          MY INFORMATION
-            <p slot="content">
-              <label class="col-sm-12 col-md-6 col-lg-3 col-xs-12">
-                <h4>{{row.productInfo[0].username}}</h4>
-                <p>
-                  <span class="address">Office Address</span>
-                  <br>
-                    <span style="font-weight:500">
-                      C-112 Girdharpark Soc.
-                    <br>
-                      Near New Era School
-                    <br>
-                      Opp. Novino Bettery
-                    <br>
-                      Makarpura, Vadodara
-                    </span>
+                </label>
+              </p>
+            </Panel>
+            <Panel name="2">
+              MY INFORMATION
+                <p slot="content">
+                  <label class="container">
+                  <h4 style="padding-right:15px;padding-left:15px">{{row.productInfo[0].username}}</h4>
+                    <div class="col-md-6">
+                          <span style="font-weight:500">
+                          NA
+                          </span>
+                    </div>
+                    <div class="col-md-6">
+                      <ul class="ulList">
+                        <li style="width:max-content">
+                          <Icon type="ios-telephone" size="15"></Icon>
+                          <span style="font-weight:500;">&nbsp;&nbsp;&nbsp;NA</span>
+                        </li>
+                        <li style="width:max-content">
+                          <Icon type="ios-email" size="15"></Icon>
+                          <span style="font-weight:500;">{{row.productInfo[0].username}}</span>
+                        </li>
+                      </ul>
+                    </div>
+                  </label>
                 </p>
-              </label>
-              <label class="col-sm-12 col-md-6 col-lg-3 col-xs-12">
-                <ul class="ulList">
-                  <li>
-                    <Icon type="ios-telephone" size="15"></Icon>
-                    <label style="font-weight:500">9978532084</label>
-                  </li>
-                  <li>
-                    <Icon type="ios-email" size="15"></Icon>
-                    <label style="font-weight:500">{{row.productInfo[0].username}}</label>
-                  </li>
-                </ul>
-              </label>
-            </p>
-          </Panel>
-      </Collapse>
+              </Panel>
+          </Collapse>
+        </div>
+        <div class="col-sm-6">
+          <div class="product-section-box">
+             <h2 class="heading-block no-tag">Post Comment</h2>
+             <div class="request-comment-info">
+                <div id="js-comment-list-ajaxresponse">
+                   <div id="style-1 js-comment-list-container" class="scrollbar">
+                      <div class="force-overflow">
+                         <ul class="request-comment-box"></ul>
+                      </div>
+                   </div>
+                </div>
+                <div class="request-post-comment">
+                   <form name="commentrequestinfo" id="commentrequestinfo" method="POST" action="/en_ca/add-comment-request-info">
+                      <div class="post-comment-text-box form-group">
+                        <textarea id="commentrequestinfo_comment" name="commentrequestinfo[comment]" required="required" class="form-control" placeholder="Please fill your comments here">
+                        </textarea>
+                        <div class="btn-box-main post-comment-btn">
+                         <!--  <input type="hidden" id="commentrequestinfo_requestInfoId" name="commentrequestinfo[requestInfoId]" class="js_myrequestid" value="25">
+                          <input type="hidden" id="commentrequestinfo_sendBy" name="commentrequestinfo[sendBy]" value="Ashish  Pandhre">
+                          <input type="hidden" id="commentrequestinfo__token" name="commentrequestinfo[_token]" value="VeSEISRZPW-iAeG06KmYE1vq15DVP6PryobLeu9ABYk"> -->
+                          <input type="button" class="btn btn-default post-pink-btn js-btn-comment-list" value="Send" data-request-id="25">
+                          <!-- <input type="button" value="Cancel" class="btn btn-default post-cancel-btn" data-dismiss="modal"> -->
+                        </div>
+                      </div>
+                   </form>
+                </div>
+             </div>
+          </div>
+        </div>
+      </div>
     </Card>
   </div>
 </template>
@@ -204,11 +229,17 @@ export default {
     .expand-row{
         margin-bottom: 16px;
     }
+    .right-border {
+      border-right: 1px #EEE solid;
+    }
+    .border-white{
+        border-right: 3px #fff solid;
+    }
+
     .order-title {
         background: #f5f5f5;
         margin: 0 0 10px;
         margin-top: 10px;
-        padding: 10px 0;
         text-align: center;
         color: #404040;
         font-size: 15px;
@@ -383,7 +414,79 @@ h3 {
     margin-right: 2px;
     margin-bottom: 2px;
 }
+ .product-section-box {
+    margin-bottom: 20px;
+    font-family: 'Roboto Condensed',sans-serif;
+    font-weight: 400;
+}
+.heading-block.no-tag {
+    padding-left: 15px;
+}
+.product-section-box h2 {
+    background: #f9f9f9;
+    font-family: 'Roboto',sans-serif;
+    font-weight: bold;
+    font-size: 16px;
+    margin-bottom: 10px;
+    padding: 8px 30px 8px 46px;
+    border-bottom: #EEE 3px solid;
+    color: #404040;
+    position: relative;
+}
+.request-comment-info {
+    padding-left: 10px;
+}
+.post-comment-text-box {
+    margin: 10px 0;
+    padding: 5px 0 0;
+    background: #eee;
+    border: solid 1px #e3e3e3;
 
+}
+.model-popup-black .form-group textarea {
+    margin-bottom: 0;
+}
+
+.form-group textarea {
+    font-family: 'Roboto Condensed',sans-serif;
+    font-weight: 400;
+    font-size: 13px;
+    color: #7f7f7f;
+    padding: 5px;
+    resize: none;
+    border-radius: 0;
+}
+.post-comment-text-box textarea {
+    width: 100%;
+    border: 1px solid #e3e3e3;
+    box-shadow: none;
+    height: 83px;
+}
+.request-post-comment input[type="button"] {
+    box-shadow: none;
+    border: 0;
+    border-radius: 3px;
+}
+.request-post-comment .post-pink-btn {
+    background: #c51327;
+    min-width: 150px;
+}
+html input[type=button] {
+    -webkit-appearance: button;
+    cursor: pointer;
+    margin: 0 10px 10px 0;
+    font-family: 'Roboto Condensed',sans-serif;
+    font-weight: 700;
+    color: #fff;
+    font-size: 14px;
+}
+.request-post-comment .post-cancel-btn {
+    background: gray;
+    min-width: 120px;
+}
+.request-post-comment .post-comment-btn {
+    text-align: right;
+}
 </style>
 
 
@@ -397,5 +500,20 @@ h3 {
   .estimate-row ul {
     list-style-type: none;
   }
-
+  .ivu-collapse>.ivu-collapse-item>.ivu-collapse-header {
+        height: 38px;
+        line-height: 38px;
+        padding-left: 32px;
+        color: #666;
+        cursor: pointer;
+        position: relative;
+        border-bottom: #EEE 3px solid;
+    }
+  /*.ivu-collapse-content>.ivu-collapse-content-box {
+        padding-top: 16px;
+    padding-bottom: 16px;
+    max-height: 400px;
+    overflow-y: auto;
+    overflow-x: hidden;
+  }*/
 </style>
