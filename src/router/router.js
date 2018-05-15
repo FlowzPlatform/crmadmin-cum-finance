2c49149f9daa894ee7cf818eab74587a1d815cb1
Hiral <hdheniya@officebrain.com>
2018-05-15 14:58:42 +0530

    checkout page for poinvoice

--- src/router/router.js
+++ src/router/router.js
@@ -221,7 +221,15 @@ export const otherRouter = {
         component: resolve => {
             require(['@/views/admin-account/change-password.vue'], resolve);
         }
-    }
+    },
+    {
+        path: 'po-checkout',
+        title: 'POCheckout',
+        name: 'POCheckout',
+        component: resolve => {
+            require(['@/views/purchaseOrder/checkout.vue'], resolve);
+        }
+    },
         // { path: 'address-book/:id',title: 'Edit Address Book', name: 'Edit Addressbook',component: resolve => { require(['@/views/my-account/edit-address-book.vue'], resolve)}}
     ]
 };
@@ -393,6 +401,7 @@ export const appRouter = [
         {
             path: '/po_invoice',
             title: 'PO Invoice',
+            name: 'PO Invoice',
             icon: 'document-text',
             name: 'PO Invoice',
             component: resolve => {
